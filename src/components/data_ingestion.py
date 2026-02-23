cb6b115e2124959a0f792f03e2310efd408a52da
AmoghaVA <amogha.va.iyer@gmail.com>
2026-02-24 01:01:37 +0530

    Model training complete

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from components.data_transformation import DataTransformation
 from components.data_transformation import DataTransformationConfig
+from components.model_trainer import ModelTrainer,ModelTrainerConfig
 from dataclasses import dataclass 
 
 # defining paths for the ingestion component to save the data
@@ -51,4 +52,7 @@ if __name__=='__main__':
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
